ff3e7d5cf927c3157567f9988aacf867bd02e6ac
Muzzamil <mujamil90@yahoo.co.in>
2022-09-25 12:53:46 +0200

    All endpoints, Json nodes and Test other data are using for common java files. Methods and Tests are updated to use this data and removed hard coded data

---
+++ src/main/java/org/api/util/CommonJsonNodes.java
@@ -0,0 +1,21 @@
+package org.api.util;
+
+
+/***
+ * @author mohammadmuzzamil
+ *
+ *
+ * This class keeps all json nodes required from Json request and response from typicode's endpoints
+ */
+
+
+public class CommonJsonNodes {
+    public static final String USER_NAME = "username";
+    public static final String ID = "id";
+    public static final String USER_ID = "userId";
+    public static final String EMAIL = "email";
+
+    public static final String CONTENT_TYPE = "Content-Type";
+
+    public static final String ACCEPT = "Accept";
+}
---
+++ src/main/java/org/api/util/Endpoints.java
@@ -0,0 +1,17 @@
+package org.api.util;
+
+/***
+ * @author mohammadmuzzamil
+ *
+ *
+ * This class keeps required endpoints for our test cases from typicode.
+ */
+
+public class Endpoints {
+
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    public static final String USERS = "/users";
+    public static final String POSTS = "/posts";
+    public static final String COMMENTS = "/comments";
+
+}
