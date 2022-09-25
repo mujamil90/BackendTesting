b35f57d681308431acc0431a2d178f4c8d32a087
Muzzamil <mujamil90@yahoo.co.in>
2022-09-25 11:48:14 +0200

    Changes tests to using the created function and remove duplicate code. Still tests nd Methods are using hard coded endpoints and test data.

--- src/test/java/org/api/tests/TypiCodeTests.java
+++ src/test/java/org/api/tests/TypiCodeTests.java
@@ -2,70 +2,36 @@ package org.api.tests;
 
 import data.PostsData;
 import org.api.util.EmailValidation;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import static io.restassured.RestAssured.given;
-import static com.jayway.restassured.path.json.JsonPath.from;
 import static org.testng.Assert.assertTrue;
-
+/***
+ * @author mohammadmuzzamil
+ */
 public class TypiCodeTests {
 
-    String userId ;
+    /***
+     * This test case will take all emails from each comment made on a post for user Samantha. All post ids are provided by data provider for user Samantha.
+     * All email will be iterated and validated for correct format explained in EmailValidation.java file.
+     * @param postId post id address the specific post
+     */
 
-    @Test
-    public void getBookingTest () {
-        String response =given ()
-                .get ("https://jsonplaceholder.typicode.com/users" )
-                .then ()
-                .statusCode (200)
-                .and ()
-                .assertThat ()
-                .log()
-                .all()
-                .extract()
-                .response().asString();
-       userId = from(response).getList("findAll{it.username=='Samantha' }.id").get(0).toString();
-
-    }
-
-
-    @Test
-    public void getPostsFromExtractedUser () {
-        given ()
-                .get ("https://jsonplaceholder.typicode.com/posts?userId=" + userId)
-                .then ()
-                .statusCode (200)
-                .and ()
-                .assertThat ()
-                .log()
-                .all()
-                .extract()
-                .response().body().path("id");
-
-
-    }
-
-
-    @Test
-    public void testGetPosts () {
-       String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
-                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    @Test(dataProvider = "getPostIds", dataProviderClass = PostsData.class)
+    public void testGetPosts (Integer postId) {
          List<String> emails = given ()
-                .get ("https://jsonplaceholder.typicode.com/posts/21/comments" )
+                .get ("https://jsonplaceholder.typicode.com/posts/"+ postId +"/comments" )
                 .then ()
                 .statusCode (200)
                 .and ()
                 .assertThat ()
                 .extract()
-                .response().body().path("email");
+                .response()
+                 .body()
+                 .path("email");
         for ( String email: emails) {
-            assertTrue( EmailValidation.patternMatches(email, regexPattern), "Email id '"+ email+ "' is not in correct format.");
+            assertTrue(EmailValidation.patternMatches(email), "Email id '"+ email+ "' is not in correct format.");
         }
 
     }
-
-
 }
